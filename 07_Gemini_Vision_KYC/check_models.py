e4c28282c7496eb998c97f056d2b7c93c3d76150
unknown <ayushyuvraj315@gmail.comgit@github.com:ayushyuvraj/finance-tracker.git>
2025-12-12 00:38:00 +0530

    Added Project 7 and Updated the README.md file

--- 07_Gemini_Vision_KYC/check_models.py
+++ 07_Gemini_Vision_KYC/check_models.py
@@ -3,7 +3,7 @@ import os
 
 # Paste your key here temporarily just to test, or set env var
 # os.environ['GOOGLE_API_KEY'] = "AIza..." 
-genai.configure(api_key="AIzaSyCw2Wr9dkNIC_X-5suLQt8QbREfwGpYx4U")
+genai.configure(api_key="Input-Your-API-Key-Here")
 
 print("Available Models:")
 for m in genai.list_models():
